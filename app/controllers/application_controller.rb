68374f4047c9e35cdb57b786c245ec4e2cd628e9
Alejandro Mangione <alejandro.mangione@gmail.com>
2016-07-27 03:47:44 -0300

    added basic authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :check_auth
+
+  def check_auth
+    authenticate_or_request_with_http_basic do |username,password|
+      users = ENV['USERS_ADMIN']
+      users.split("|").each do |user|
+        if user.split(',').first == username && user.split(',').last == password
+          session['login'] = user.split(',').first
+          true
+        end
+      end
+    end
+  end
+
 end
